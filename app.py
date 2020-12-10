ac38baa71f19ae2672c7fe7b5bca77fc07bd8d0a
cookie <a0984229157@gmail.com>
2020-12-10 14:58:51 +0800

    make it better

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def query_user(name):
             for i in users:
                 i.pop("_id")
                 x.append(i)
-            return name
+            return jsonify(x)
     else:
         return 'No user found!'
 #test to insert data to the data base
