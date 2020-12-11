ff4d66c0b571b52bc57f5d6fb69d3a9e04fb28cc
cookie <a0984229157@gmail.com>
2020-12-11 22:53:12 +0800

    make it better

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def query_event(driver_id):
                 
                 result=[]
                 if i["status"]=="white":
-                    return i["event_id"]
+                    return type(i["event_id"])
                     # request=db.request_collection.find({"event_id": i["event_id"]})
                     # for j in request:
                     #     j.pop("_id")
