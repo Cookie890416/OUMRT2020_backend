946cb24c2d09fb6d9c5db54c82ae3fed402f93ff
cookie <a0984229157@gmail.com>
2020-12-14 00:35:16 +0800

    make it better

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def flask_mongodb_atlas():
     return "flask mongodb atlas!"
 @app.route('/test-field',methods=['POST'])
 def showField():
-    return request.text
+    return request.data
 @app.route('/query_driver/<string:driver_id>')
 def query_driverevent(driver_id):
     if driver_id:
