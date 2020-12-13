e3acedb8fa37fb62cffdc6f097ea0f0b55a30998
cookie <a0984229157@gmail.com>
2020-12-13 16:29:00 +0800

    make it better

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ def query_driverevent(driver_id):
                     j={}
                     for j in db.request_collection.find({"event_id": i["event_id"]}):
                         j.pop("_id")
-                        
+                        for k in db.user_collection.find({"user_id": j["user_id"]}):
                             
-                        i['all_request']=j
+                            i['all_request']=k
                     i.update({"reason":None,"final_request":None,"user":None})
                     x.append(i)
                     return jsonify(x)
