c768902233d140d7eed400db6711fa320e1f3e53
Derek Yang <43636582+dkyg1087@users.noreply.github.com>
2021-01-03 20:15:44 +0800

    fix inform bug

--- getInform.py
+++ getInform.py
@@ -10,5 +10,8 @@ def getUserInform():
     requestObj = request.form
     userID = requestObj['user_id']
     userInform = mongo.inform_collection.find_one({"user_id":userID})
-    result = {"driver_context":userInform['driver_context'],"passenger_context":userInform['passenger_context']}
+    if userInform is None:
+        result = {"driver_context":[],"passenger_context":[]}
+    else:
+        result = {"driver_context":userInform['driver_context'],"passenger_context":userInform['passenger_context']}
     return jsonify(result)
